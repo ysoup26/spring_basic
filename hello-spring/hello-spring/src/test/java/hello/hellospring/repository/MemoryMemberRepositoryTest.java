15dc500f4af9b818f3837a17399b150aaf3300a9
ysoup26 <ysoup26@gmail.com>
2024-05-10 23:33:07 +0900

    테스트케이스 작성
    member가 담긴 Map을 매번 초기화해서
    테스트케이스 실행에 문제가 없게함

--- hello-spring/hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ hello-spring/hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -1,11 +1,22 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class MemoryMemberRepositoryTest {
-    MemberRepository repository = new MemoryMemberRepository();
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
 
     @Test
     public void save(){
@@ -13,10 +24,38 @@ public class MemoryMemberRepositoryTest {
         member.setName("spring");
         repository.save(member);
         Member result = repository.findById(member.getId()).get();
-        Assertions.assertEquals(member,result);
-        //Assertions.assertThat(member).isEqualTo(result);/*버전 차이로 인한 문제가 있는듯*/
 
-        //assertThat(member,is(equalsTo(result)));
+        //Assertions.assertThat(member).isEqualTo(result);/*버전 차이로 인한 문제가 있는듯*/
+        assertThat(member, equalTo(result));
+        //assertThat(member.getName(), is(equalTo("spring")));
+
         //System.out.println("result = "+(result==member));
+    }
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring2").get();
+        assertThat(result,is(equalTo(member2)));
+    }
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result= repository.findAll();
+
+        assertThat(result.size(),is(equalTo(2)));
     }
 }
